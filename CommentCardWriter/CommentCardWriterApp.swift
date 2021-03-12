8acb22e88e7933de08f5a45c7bc4827a51f9ad6d
Saahil <bahadur.s@etoncollege.org.uk>
2021-03-12 12:24:31 +0000

    Initial Commit

---
+++ CommentCardWriter/CommentCardWriterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommentCardWriterApp.swift
+//  CommentCardWriter
+//
+//  Created by Bahadur, Saahil (PAH) on 12/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct CommentCardWriterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CommentCardWriter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CommentCardWriter
+//
+//  Created by Bahadur, Saahil (PAH) on 12/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
